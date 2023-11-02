5dc2ea998b11b27edecb4c34371f9eb103b0e631
EriCrysBel <erikacrystal95@gmail.com>
2023-11-02 10:57:57 +0100

    ajuste del container

--- script.js
+++ script.js
@@ -16,3 +16,35 @@ tabLinks.forEach(
 $('.message a').click(function(){
   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   // Seleccionar todas las pestañas y contenidos
+//   let tabLinks = document.querySelectorAll('.tab-link');
+//   let tabContents = document.querySelectorAll('.tab-content');
+
+//   // Función para ocultar todos los contenidos de las pestañas
+//   function hideTabContent() {
+//       tabContents.forEach(function (tabContent) {
+//           tabContent.style.display = 'none';
+//       });
+//   }
+
+//   // Función para mostrar el contenido de una pestaña
+//   function showTabContent(id) {
+//       let tabContent = document.querySelector(`#${id}`);
+//       tabContent.style.display = 'block';
+//   }
+
+//   // Ocultar todos los contenidos de las pestañas al inicio
+//   hideTabContent();
+
+//   // Agregar eventos a los enlaces de las pestañas
+//   tabLinks.forEach(function (tabLink) {
+//       tabLink.addEventListener('click', function (event) {
+//           event.preventDefault();
+//           let id = event.target.getAttribute('.tab-link');
+//           hideTabContent();
+//           showTabContent(id);
+//       });
+//   });
+// });
